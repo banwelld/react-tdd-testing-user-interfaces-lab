ae18325a7458929751bf202177b1b5d01c5ff17f
Dave Banwell <dave@davebanwell.com>
2024-09-29 19:59:49 -0400

    all tests work and pass

--- src/App.js
+++ src/App.js
@@ -1,5 +1,44 @@
 function App() {
-  return <div>Your code here</div>;
+
+  return (
+    <div style={{margin: '0 15%'}}>
+      <div>
+        <h1 style={{fontSize: '48px', fontFamily: 'Helvetica'}}>Hi, I'm Dave</h1>
+      </div>
+      <div style={{display: 'flex', flexDirection: 'row'}}>
+        <span style={{width: '40%', display: 'flex', flexDirection: 'column', alignContent: 'center', justifyContent: 'center', textAlign: 'center'}}>
+          <img
+            src="/propic.jpg" 
+            alt="Dave Banwell in alleyway with red, white, black, and grey grafiti" 
+            style={{width: '300px', height: '300px', borderRadius: '50%', objectFit: 'cover', alignSelf: 'left'}}
+          />
+        </span>
+        <span style={{width: '60%', display: 'flex', flexDirection: 'column', justifyContent: 'center', textAlign: 'left'}}>
+          <h2
+            style={{fontFamily: 'Helvetica'}}
+          >
+            About Me
+          </h2>
+          <p>
+            Dave Banwell is a queer, autistic, world traveller, and lover of life. He works as a speech analyst and 
+            data visualization specialist, while also spending time singing opera, learning software engineering and
+            coaching others in overcoming the obstacles in their lives. Dave is a passionate advocate of equity-deserving
+            people from all backgrounds, and of humanity, generally, with the goal of expediting human progress and 
+            exploring all realms of what's possible when we're united as a people.
+          </p>
+          <div style={{display: 'flex', flexDirection: 'row', gap: '20px', justifyContent: 'center'}}>
+            <a href="https://github.com/banwelld">
+              <button>Visit my Github page</button>
+            </a>
+            <a href="https://www.linkedin.com/in/dave-banwell-2b7a6511">
+              <button>Visit my LinkedIn page</button>
+            </a>
+          </div>
+        </span>
+      </div>
+    </div>
+  );
+
 }
 
 export default App;
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,6 +1,56 @@
 import { render, screen } from "@testing-library/react";
 import '@testing-library/jest-dom';
-
 import App from "../App";
 
-// Your tests here
+test('Displays a top-level heading with the text \'Hi, I\'m Dave', () => {
+    render(<App />);
+    const topLevelHeading = screen.getByRole('heading', {
+        name: /hi, i'm/i,
+        exact: false,
+        level: 1,
+    });
+    expect(topLevelHeading).toBeInTheDocument();
+});
+
+test('Displays an image of the page owner with alt attribute to describe', () => {
+    render(<App />);
+    const ownerImage = screen.getByRole('img', {
+        alt: /dave/i,
+    });
+    expect(ownerImage).toBeInTheDocument();
+    expect(ownerImage).toHaveAttribute('src');
+});
+
+test('Displays a second-level heading with the text \'About Me\'.', () => {
+    render(<App />);
+    const aboutMeHeading = screen.getByRole('heading', {
+        name: /about me/i,
+        exact: false,
+        level: 2,
+    });
+    expect(aboutMeHeading).toBeInTheDocument();
+});
+
+test('Displays a paragraph comprising a mini-biography of the page owner', () => {
+    render(<App />);
+    const aboutMeHeading = screen.getByRole('heading', {
+        name: /about me/i,
+        exact: false,
+        level: 2,
+    });
+    const miniBio = aboutMeHeading.nextElementSibling;
+    expect(miniBio.tagName).toBe('P');
+    expect(miniBio.textContent).toMatch(/dave/i);
+    expect(miniBio.textContent.length).toBeGreaterThan(50);
+});
+
+test('Displays two links to GitHub and LinkedIn pages', () => {
+    render(<App />);
+    const links = screen.getAllByRole('link');
+    expect(links).toHaveLength(2);
+
+    const githubLink = links.find(link => link.href.includes('github.com/'));
+    const linkedinLink = links.find(link => link.href.includes('linkedin.com/'));
+    expect(githubLink).toBeInTheDocument();
+    expect(linkedinLink).toBeInTheDocument();
+  });
